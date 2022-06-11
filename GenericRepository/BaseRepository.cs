d9d0b9efeec8bd268827d6bf8c97024b0cd45d89
Михаил <sklm@spaces.ru>
2022-06-11 16:45:45 +0300

    Refactoring

--- GenericRepository/BaseRepository.cs
+++ GenericRepository/BaseRepository.cs
@@ -1,26 +1,18 @@
 ﻿using GenericRepository.Enums;
-using GenericRepository.Models.Sorting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
 using System.Linq.Expressions;
-using GenericRepository.Enums.Sorting;
-using System.Reflection;
 
 namespace GenericRepository;
 
-public class BaseRepository<TEntity> where TEntity : class
+public class BaseRepository<TEntity> : IDisposable
+    where TEntity : class
 {
     private readonly DbContext _context;
 
     public BaseRepository(DbContext context)
     {
         _context = context;
-    }
-
-    public async Task<TEntity> AddAsync(TEntity entity, CancellationToken token = default)
-    {
-        var task = await GetDbSet(TrackingMode.TrackAll).AddAsync(entity, token);
-        return task.Entity;
     }
 
     public Task AddAsync(IEnumerable<TEntity> entities, CancellationToken token = default) =>
@@ -34,140 +26,24 @@ public class BaseRepository<TEntity> where TEntity : class
 
     public void PhysicalDelete(IEnumerable<TEntity> entities) => GetDbSet(TrackingMode.TrackAll).RemoveRange(entities);
 
-    public Task<TEntity[]> SelectByConditionAsync(Expression<Func<TEntity, bool>> predicate,
-        CancellationToken token = default,
-        TrackingMode tracking = TrackingMode.NoTracking)
-    {
-        return GetBaseQuery(tracking)
-            .Where(predicate)
-            .ToArrayAsync(token);
-    }
-
-    public Task<TEntity[]> SelectByConditionAsync(Expression<Func<TEntity, bool>> predicate,
-        IEnumerable<SortingParameter> sorting,
-        CancellationToken token = default,
-        TrackingMode tracking = TrackingMode.NoTracking)
-    {
-        var filteredQuery = GetBaseQuery(tracking).Where(predicate);
-        var sorteredQuery = Sort(filteredQuery, sorting);
-
-        return sorteredQuery.ToArrayAsync(token);
-    }
-
-    public Task<TEntity[]> SelectByConditionAsync(Expression<Func<TEntity, bool>> predicate,
-        CancellationToken token = default,
-        TrackingMode tracking = TrackingMode.NoTracking,
-        params Expression<Func<TEntity, object>>[] includes)
-    {
-        return GetBaseQuery(tracking, includes)
-            .Where(predicate)
-            .ToArrayAsync(token);
-    }
-
-    public Task<TEntity[]> SelectByConditionAsync(Expression<Func<TEntity, bool>> predicate,
-        IEnumerable<SortingParameter> sorting,
-        CancellationToken token = default,
-        TrackingMode tracking = TrackingMode.NoTracking,
-        params Expression<Func<TEntity, object>>[] includes)
-    {
-        var filteredQuery = GetBaseQuery(tracking, includes).Where(predicate);
-        var sorteredQuery = Sort(filteredQuery, sorting);
-        return sorteredQuery.ToArrayAsync(token);
-    }
-
-    public Task<TEntity[]> SelectByConditionAsync(Expression<Func<TEntity, bool>> predicate,
-        CancellationToken token = default,
-        TrackingMode tracking = TrackingMode.NoTracking,
-        params Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>[] includes)
-    {
-        return GetBaseQueryWithIncludes(tracking, includes)
-            .Where(predicate)
-            .ToArrayAsync(token);
-    }
-
-    public Task<TEntity[]> SelectByConditionIncludeAsync(Expression<Func<TEntity, bool>> predicate,
-        IEnumerable<SortingParameter> sorting,
-        CancellationToken token = default,
-        TrackingMode tracking = TrackingMode.NoTracking,
-        params Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>[] includes)
-    {
-        var filteredQuery = GetBaseQueryWithIncludes(tracking, includes).Where(predicate);
-        var sorteredQuery = Sort(filteredQuery, sorting);
-        return sorteredQuery.ToArrayAsync(token);
-    }
-
-    public Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate,
-        CancellationToken token = default,
-        TrackingMode tracking = TrackingMode.NoTracking)
-    {
-        return GetBaseQuery(tracking)
-            .Where(predicate)
-            .FirstOrDefaultAsync(token);
-    }
-
-    public Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate,
-        CancellationToken token = default,
-        TrackingMode tracking = TrackingMode.NoTracking,
-        params Expression<Func<TEntity, object>>[] includes)
-    {
-        return GetBaseQuery(tracking, includes)
-            .FirstOrDefaultAsync(predicate, token);
-    }
-
-    public Task<TEntity?> FirstOrDefaultIncludeAsync(Expression<Func<TEntity, bool>> predicate,
-        CancellationToken token = default,
-        TrackingMode tracking = TrackingMode.NoTracking,
-        params Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>[] includes)
-    {
-        return GetBaseQueryWithIncludes(tracking, includes)
-            .FirstOrDefaultAsync(predicate, token);
-    }
-
-    public Task<TEntity?> SingleOrDefaultAsync(TrackingMode tracking = TrackingMode.NoTracking,
-        CancellationToken token = default,
-        params Expression<Func<TEntity, object>>[] includes)
-    {
-        return GetBaseQuery(tracking, includes)
-            .SingleOrDefaultAsync(token);
-    }
-
-    public Task<TEntity?> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate,
-        CancellationToken token = default,
-        TrackingMode tracking = TrackingMode.NoTracking,
-        params Expression<Func<TEntity, object>>[] includes)
-    {
-        return GetBaseQuery(tracking, includes)
-            .SingleOrDefaultAsync(predicate, token);
-    }
-
-    public Task<TEntity?> SingleOrDefaultIncludeAsync(Expression<Func<TEntity, bool>> predicate,
-        CancellationToken token = default,
-        TrackingMode tracking = TrackingMode.NoTracking,
-        params Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>[] includes)
-    {
-        return GetBaseQueryWithIncludes(tracking, includes)
-            .SingleOrDefaultAsync(predicate, token);
-    }
-
-    public Task<int> CountAsync(CancellationToken token = default) => GetDbSet().CountAsync(token);
-
-    public Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken token = default) =>
-        GetDbSet().CountAsync(predicate, token);
-
-    public Task<bool> AnyAsync(CancellationToken token = default) => GetDbSet().AnyAsync(token);
-
-    public Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken token = default) =>
-        GetDbSet().AnyAsync(predicate, token);
-
     public virtual Task<int> SaveChangesAsync(CancellationToken token = default)
     {
         return _context.SaveChangesAsync(token);
     }
 
-    private IQueryable<TEntity> GetBaseQuery(TrackingMode trackingMode = TrackingMode.NoTracking) =>
+    protected DbSet<TEntity> GetDbSet(TrackingMode trackingMode = TrackingMode.NoTracking)
+    {
+        _context.ChangeTracker.QueryTrackingBehavior = (trackingMode == TrackingMode.TrackAll)
+            ? QueryTrackingBehavior.TrackAll
+            : QueryTrackingBehavior.NoTracking;
+
+        return _context.Set<TEntity>();
+    }
+
+    protected IQueryable<TEntity> GetBaseQuery(TrackingMode trackingMode = TrackingMode.NoTracking) =>
         GetDbSet(trackingMode);
 
-    private IQueryable<TEntity> GetBaseQuery(TrackingMode trackingMode = TrackingMode.NoTracking,
+    protected IQueryable<TEntity> GetBaseQuery(TrackingMode trackingMode = TrackingMode.NoTracking,
         params Expression<Func<TEntity, object>>[] includes)
     {
         var query = GetBaseQuery(trackingMode);
@@ -180,7 +56,7 @@ public class BaseRepository<TEntity> where TEntity : class
         return query;
     }
 
-    private IQueryable<TEntity> GetBaseQueryWithIncludes(TrackingMode trackingMode = TrackingMode.NoTracking,
+    protected IQueryable<TEntity> GetBaseQueryWithIncludes(TrackingMode trackingMode = TrackingMode.NoTracking,
         params Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>[] includes)
     {
         var query = GetBaseQuery(trackingMode);
@@ -192,56 +68,6 @@ public class BaseRepository<TEntity> where TEntity : class
 
         return query;
     }
-
-    private DbSet<TEntity> GetDbSet(TrackingMode trackingMode = TrackingMode.NoTracking)
-    {
-        _context.ChangeTracker.QueryTrackingBehavior = (trackingMode == TrackingMode.TrackAll)
-            ? QueryTrackingBehavior.TrackAll
-            : QueryTrackingBehavior.NoTracking;
-
-        return _context.Set<TEntity>();
-    }
-
-    private IQueryable<TEntity> Sort(IQueryable<TEntity> query, IEnumerable<SortingParameter> sortingParameters)
-    {
-        var result = query;
-        if (sortingParameters?.Any() == true)
-        {
-            var type = typeof(TEntity);
-            var orderByCommand = sortingParameters.First().Direction == SortDirection.Ascending
-                ? "OrderBy"
-                : "OrderByDescending";
-
-            var isOrderedQuery = false;
-            foreach (var sorting in sortingParameters)
-            {
-                var property =
-                    type.GetProperty(sorting.FieldName, BindingFlags.FlattenHierarchy
-                    | BindingFlags.Instance
-                    | BindingFlags.Public);
-
-                if (property == null)
-                {
-                    throw new Exception($"Property {sorting.FieldName} is not found");
-                }
-                if (isOrderedQuery)
-                {
-                    orderByCommand = sorting.Direction == SortDirection.Ascending
-                        ? "ThenBy"
-                        : "ThenByDescending";
-                }
-                var parameter = Expression.Parameter(type, "p");
-                var propertyAccess = Expression.MakeMemberAccess(parameter, property);
-                var orderByExpression = Expression.Lambda(propertyAccess, parameter);
-                var resultExpression = Expression.Call(typeof(Queryable), orderByCommand, new Type[] { type, property.PropertyType },
-                    result.Expression, Expression.Quote(orderByExpression));
-                result = result.Provider.CreateQuery<TEntity>(resultExpression);
-                isOrderedQuery = true;
-            }
-        }
-        return result;
-    }
-
     public void Dispose()
     {
         _context.Dispose();
---
+++ GenericRepository/Repository.cs
@@ -0,0 +1,186 @@
+﻿using GenericRepository.Enums;
+using GenericRepository.Enums.Sorting;
+using GenericRepository.Models.Sorting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace GenericRepository;
+
+public class Repository<TEntity> : BaseRepository<TEntity>
+    where TEntity : class
+{
+    public Repository(DbContext context) : base(context)
+    {}
+
+    public async Task<TEntity> AddAsync(TEntity entity, CancellationToken token = default)
+    {
+        var task = await GetDbSet(TrackingMode.TrackAll).AddAsync(entity, token);
+        return task.Entity;
+    }
+    public Task<TEntity[]> SelectByConditionAsync(Expression<Func<TEntity, bool>> predicate,
+        CancellationToken token = default,
+        TrackingMode tracking = TrackingMode.NoTracking)
+    {
+        return GetBaseQuery(tracking)
+            .Where(predicate)
+            .ToArrayAsync(token);
+    }
+
+    public Task<TEntity[]> SelectByConditionAsync(Expression<Func<TEntity, bool>> predicate,
+        IEnumerable<SortingParameter> sorting,
+        CancellationToken token = default,
+        TrackingMode tracking = TrackingMode.NoTracking)
+    {
+        var filteredQuery = GetBaseQuery(tracking).Where(predicate);
+        var sorteredQuery = Sort(filteredQuery, sorting);
+
+        return sorteredQuery.ToArrayAsync(token);
+    }
+
+    public Task<TEntity[]> SelectByConditionAsync(Expression<Func<TEntity, bool>> predicate,
+        CancellationToken token = default,
+        TrackingMode tracking = TrackingMode.NoTracking,
+        params Expression<Func<TEntity, object>>[] includes)
+    {
+        return GetBaseQuery(tracking, includes)
+            .Where(predicate)
+            .ToArrayAsync(token);
+    }
+
+    public Task<TEntity[]> SelectByConditionAsync(Expression<Func<TEntity, bool>> predicate,
+        IEnumerable<SortingParameter> sorting,
+        CancellationToken token = default,
+        TrackingMode tracking = TrackingMode.NoTracking,
+        params Expression<Func<TEntity, object>>[] includes)
+    {
+        var filteredQuery = GetBaseQuery(tracking, includes).Where(predicate);
+        var sorteredQuery = Sort(filteredQuery, sorting);
+        return sorteredQuery.ToArrayAsync(token);
+    }
+
+    public Task<TEntity[]> SelectByConditionAsync(Expression<Func<TEntity, bool>> predicate,
+        CancellationToken token = default,
+        TrackingMode tracking = TrackingMode.NoTracking,
+        params Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>[] includes)
+    {
+        return GetBaseQueryWithIncludes(tracking, includes)
+            .Where(predicate)
+            .ToArrayAsync(token);
+    }
+
+    public Task<TEntity[]> SelectByConditionIncludeAsync(Expression<Func<TEntity, bool>> predicate,
+        IEnumerable<SortingParameter> sorting,
+        CancellationToken token = default,
+        TrackingMode tracking = TrackingMode.NoTracking,
+        params Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>[] includes)
+    {
+        var filteredQuery = GetBaseQueryWithIncludes(tracking, includes).Where(predicate);
+        var sorteredQuery = Sort(filteredQuery, sorting);
+        return sorteredQuery.ToArrayAsync(token);
+    }
+
+    public Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate,
+        CancellationToken token = default,
+        TrackingMode tracking = TrackingMode.NoTracking)
+    {
+        return GetBaseQuery(tracking)
+            .Where(predicate)
+            .FirstOrDefaultAsync(token);
+    }
+
+    public Task<TEntity?> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate,
+        CancellationToken token = default,
+        TrackingMode tracking = TrackingMode.NoTracking,
+        params Expression<Func<TEntity, object>>[] includes)
+    {
+        return GetBaseQuery(tracking, includes)
+            .FirstOrDefaultAsync(predicate, token);
+    }
+
+    public Task<TEntity?> FirstOrDefaultIncludeAsync(Expression<Func<TEntity, bool>> predicate,
+        CancellationToken token = default,
+        TrackingMode tracking = TrackingMode.NoTracking,
+        params Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>[] includes)
+    {
+        return GetBaseQueryWithIncludes(tracking, includes)
+            .FirstOrDefaultAsync(predicate, token);
+    }
+
+    public Task<TEntity?> SingleOrDefaultAsync(TrackingMode tracking = TrackingMode.NoTracking,
+        CancellationToken token = default,
+        params Expression<Func<TEntity, object>>[] includes)
+    {
+        return GetBaseQuery(tracking, includes)
+            .SingleOrDefaultAsync(token);
+    }
+
+    public Task<TEntity?> SingleOrDefaultAsync(Expression<Func<TEntity, bool>> predicate,
+        CancellationToken token = default,
+        TrackingMode tracking = TrackingMode.NoTracking,
+        params Expression<Func<TEntity, object>>[] includes)
+    {
+        return GetBaseQuery(tracking, includes)
+            .SingleOrDefaultAsync(predicate, token);
+    }
+
+    public Task<TEntity?> SingleOrDefaultIncludeAsync(Expression<Func<TEntity, bool>> predicate,
+        CancellationToken token = default,
+        TrackingMode tracking = TrackingMode.NoTracking,
+        params Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>[] includes)
+    {
+        return GetBaseQueryWithIncludes(tracking, includes)
+            .SingleOrDefaultAsync(predicate, token);
+    }
+
+    public Task<int> CountAsync(CancellationToken token = default) => GetDbSet().CountAsync(token);
+
+    public Task<int> CountAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken token = default) =>
+        GetDbSet().CountAsync(predicate, token);
+
+    public Task<bool> AnyAsync(CancellationToken token = default) => GetDbSet().AnyAsync(token);
+
+    public Task<bool> AnyAsync(Expression<Func<TEntity, bool>> predicate, CancellationToken token = default) =>
+        GetDbSet().AnyAsync(predicate, token);
+
+    private IQueryable<TEntity> Sort(IQueryable<TEntity> query, IEnumerable<SortingParameter> sortingParameters)
+    {
+        var result = query;
+        if (sortingParameters?.Any() == true)
+        {
+            var type = typeof(TEntity);
+            var orderByCommand = sortingParameters.First().Direction == SortDirection.Ascending
+                ? "OrderBy"
+                : "OrderByDescending";
+
+            var isOrderedQuery = false;
+            foreach (var sorting in sortingParameters)
+            {
+                var property =
+                    type.GetProperty(sorting.FieldName, BindingFlags.FlattenHierarchy
+                    | BindingFlags.Instance
+                    | BindingFlags.Public);
+
+                if (property == null)
+                {
+                    throw new Exception($"Property {sorting.FieldName} is not found");
+                }
+                if (isOrderedQuery)
+                {
+                    orderByCommand = sorting.Direction == SortDirection.Ascending
+                        ? "ThenBy"
+                        : "ThenByDescending";
+                }
+                var parameter = Expression.Parameter(type, "p");
+                var propertyAccess = Expression.MakeMemberAccess(parameter, property);
+                var orderByExpression = Expression.Lambda(propertyAccess, parameter);
+                var resultExpression = Expression.Call(typeof(Queryable), orderByCommand, new Type[] { type, property.PropertyType },
+                    result.Expression, Expression.Quote(orderByExpression));
+                result = result.Provider.CreateQuery<TEntity>(resultExpression);
+                isOrderedQuery = true;
+            }
+        }
+        return result;
+    }
+}
